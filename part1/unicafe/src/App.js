95398ab0af76477871a7b3ce592ab42dbea536f3
aamircodes <aamirp1@outlook.com>
2022-04-06 23:48:59 +0100

    Exercise 1.8-1.9

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -8,6 +8,22 @@ const Header = ({ text }) => (
 
 const Button = (props) => <button onClick={props.handleClick}>{props.text}</button>
 
+const Statistics = ({ good, neutral, bad, all, average, positive }) => {
+  if (!all) {
+    return <div>No feedback given</div>
+  }
+  return (
+    <div>
+      <div>good {good}</div>
+      <div>neutral {neutral}</div>
+      <div>bad {bad}</div>
+      <div>all {all}</div>
+      <div>average {average} </div>
+      <div>positive {positive} %</div>
+    </div>
+  )
+}
+
 const App = () => {
   // save clicks of each button to its own state
   const [good, setGood] = useState(0)
@@ -22,12 +38,26 @@ const App = () => {
       <Button handleClick={() => setBad(bad + 1)} text='bad' />
       <Header text='statistics' />
       <div>
-        <p>good {good}</p>
+        <Statistics
+          good={good}
+          neutral={neutral}
+          bad={bad}
+          all={good + neutral + bad}
+          average={(good - bad) / (good + bad + neutral)}
+          positive={(good / (good + neutral + bad)) * 100}
+        />
+        {/* <Statistics text='good' statistic={good} />
+        <Statistics text='neutral' statistic={neutral} />
+        <Statistics text='bad' statistic={bad} />
+        <Statistics text='all' statistic={good + neutral + bad} />
+        <Statistics text='average' statistic={(good - bad) / (good + bad + neutral)} />
+        <Statistics text='positive' statistic={(good / (good + neutral + bad)) * 100 + ' %'} /> */}
+        {/* <p>good {good}</p>
         <p>neutral {neutral}</p>
         <p>bad {bad}</p>
         <p>all {good + neutral + bad}</p>
-        <p>average {(good - bad) / (good + bad + neutral)} </p>
-        <p>positive {(good / (good + neutral + bad)) * 100} %</p>
+        <p>average {} </p>
+        <p>positive {(good / (good + neutral + bad)) * 100} %</p> */}
       </div>
     </div>
   )
