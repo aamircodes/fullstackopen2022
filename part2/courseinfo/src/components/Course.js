68ea0515971cc4e46b8b46d616fd10cfcc676e95
aamircodes <aamirp1@outlook.com>
2022-04-08 01:02:34 +0100

    Exercise 2.4-2.5

--- part2/courseinfo/src/components/Course.js
+++ part2/courseinfo/src/components/Course.js
@@ -1,14 +1,42 @@
 import React from 'react'
-import Header from './Header'
-import Content from './Content'
-import Total from './Total'
 
-const Course = (props) => {
+const Header = ({ course }) => {
+  return <h3>{course.name}</h3>
+}
+
+const Part = (props) => {
+  return (
+    <p>
+      {props.part.name} {props.part.exercises}
+    </p>
+  )
+}
+
+const Content = ({ course }) => {
   return (
     <div>
-      <Header course={props.course} />
-      <Content course={props.course} />
-      <Total course={props.course} />
+      {course.parts.map((part, i) => (
+        <Part key={i} part={part} />
+      ))}
+    </div>
+  )
+}
+
+const Course = ({ course }) => {
+  return (
+    <>
+      <Header course={course} />
+      <Content course={course} />
+      <Total course={course} />
+    </>
+  )
+}
+
+const Total = ({ course }) => {
+  const sum = course.parts.reduce((total, part) => total + part.exercises, 0)
+  return (
+    <div>
+      <b>total of {sum} exercises</b>
     </div>
   )
 }
