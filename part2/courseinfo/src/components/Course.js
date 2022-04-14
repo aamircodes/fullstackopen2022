3a93cb9e78cb7128805c36517776726da3578e7d
aamircodes <aamirp1@outlook.com>
2022-04-14 01:02:54 +0100

    redo 2.1

--- part2/courseinfo/src/components/Course.js
+++ part2/courseinfo/src/components/Course.js
@@ -1,42 +1,13 @@
 import React from 'react'
+import Header from './Header'
+import Content from './Content'
 
-const Header = ({ course }) => {
-  return <h3>{course.name}</h3>
-}
-
-const Part = (props) => {
-  return (
-    <p>
-      {props.part.name} {props.part.exercises}
-    </p>
-  )
-}
-
-const Content = ({ course }) => {
+const Course = (props) => {
+  const { course } = props
   return (
     <div>
-      {course.parts.map((part, i) => (
-        <Part key={i} part={part} />
-      ))}
-    </div>
-  )
-}
-
-const Course = ({ course }) => {
-  return (
-    <>
-      <Header course={course} />
-      <Content course={course} />
-      <Total course={course} />
-    </>
-  )
-}
-
-const Total = ({ course }) => {
-  const sum = course.parts.reduce((total, part) => total + part.exercises, 0)
-  return (
-    <div>
-      <b>total of {sum} exercises</b>
+      <Header course={course.name} />
+      <Content parts={course.parts} />
     </div>
   )
 }
