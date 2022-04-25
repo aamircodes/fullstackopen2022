3b7f75ae6b2e1c3d147c617c821ff13f22794038
aamircodes <aamirp1@outlook.com>
2022-04-25 18:30:39 +0100

    3.9

--- part2/phonebook/src/services/person.js
+++ part2/phonebook/src/services/person.js
@@ -1,29 +1,30 @@
-import axios from 'axios'
-const baseUrl = 'http://localhost:3001/persons'
+import axios from 'axios';
+
+const baseUrl = 'http://localhost:3001/api/persons';
 
 const getAll = () => {
-  const req = axios.get(baseUrl)
-  return req.then((res) => res.data)
-}
+  const req = axios.get(baseUrl);
+  return req.then((res) => res.data);
+};
 
 const create = (newObj) => {
-  const req = axios.post(baseUrl, newObj)
-  return req.then((res) => res.data)
-}
+  const req = axios.post(baseUrl, newObj);
+  return req.then((res) => res.data);
+};
 
 const deleteIt = (id) => {
-  const request = axios.delete(`${baseUrl}/${id}`)
-  return request.then((response) => response.data)
-}
+  const request = axios.delete(`${baseUrl}/${id}`);
+  return request.then((response) => response.data);
+};
 
 const update = (id, newObject) => {
-  const request = axios.put(`${baseUrl}/${id}`, newObject)
-  return request.then((response) => response.data)
-}
+  const request = axios.put(`${baseUrl}/${id}`, newObject);
+  return request.then((response) => response.data);
+};
 
 export default {
   getAll,
   create,
   deleteIt,
   update,
-}
+};
