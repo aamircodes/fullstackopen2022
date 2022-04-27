aa2baff51d0e0b021a1098ed0dde63e5af222d1f
aamircodes <aamirp1@outlook.com>
2022-04-27 13:13:06 +0100

    part3-backend

--- part3-backend/index.js
+++ part3-backend/index.js
@@ -4,6 +4,8 @@ const cors = require('cors');
 require('dotenv').config();
 const Note = require('./models/note');
 
+app.use(cors());
+
 const requestLogger = (request, response, next) => {
   console.log('Method:', request.method);
   console.log('Path:  ', request.path);
@@ -12,13 +14,9 @@ const requestLogger = (request, response, next) => {
   next();
 };
 
-app.use(express.json());
-
-app.use(requestLogger);
-
-app.use(cors());
-
 app.use(express.static('build'));
+app.use(express.json());
+app.use(requestLogger);
 
 app.get('/', (req, res) => {
   res.send('<h1>Hello World!</h1>');
@@ -48,6 +46,18 @@ app.get('/api/notes', (request, response) => {
   });
 });
 
+app.get('/api/notes/:id', (request, response, next) => {
+  Note.findById(request.params.id)
+    .then((note) => {
+      if (note) {
+        response.json(note);
+      } else {
+        response.status(404).end();
+      }
+    })
+    .catch((error) => next(error));
+});
+
 app.delete('/api/notes/:id', (request, response) => {
   const id = Number(request.params.id);
   notes = notes.filter((note) => note.id !== id);
@@ -55,17 +65,23 @@ app.delete('/api/notes/:id', (request, response) => {
   response.status(204).end();
 });
 
-app.get('/api/notes/:id', (request, response) => {
-  Note.findById(request.params.id).then((note) => {
-    response.json(note);
-  });
-});
-
 const unknownEndpoint = (request, response) => {
   response.status(404).send({ error: 'unknown endpoint' });
 };
 
 app.use(unknownEndpoint);
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message);
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' });
+  }
+
+  next(error);
+};
+
+app.use(errorHandler);
 
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
