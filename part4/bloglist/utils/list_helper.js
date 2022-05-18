c50365f2ae1bc82e217c0560b42dc2302b4d7ded
aamircodes <aamirp1@outlook.com>
2022-05-18 20:30:06 +0100

    4.5

--- part4/bloglist/utils/list_helper.js
+++ part4/bloglist/utils/list_helper.js
@@ -6,7 +6,12 @@ const totalLikes = (blogs) => {
   return blogs.length === 0 ? 0 : blogs.reduce((sum, blog) => sum + blog.likes, 0)
 }
 
+const favoriteBlog = (blogs) => {
+  return blogs.length === 0 ? {} : blogs.reduce((lastBlog, currentBlog) => (currentBlog.likes > lastBlog.likes ? currentBlog : lastBlog))
+}
+
 module.exports = {
   dummy,
   totalLikes,
+  favoriteBlog,
 }
