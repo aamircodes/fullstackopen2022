21d5107a0950ec48a27f977f6ec4a6e552b19ce6
aamircodes <aamirp1@outlook.com>
2022-05-31 11:29:24 +0100

    5.1

--- part5/src/App.js
+++ part5/src/App.js
@@ -57,22 +57,14 @@ const App = () => {
         <Notification message={errorMessage} />
         <h2>Log in to application</h2>
         {loginForm()}
-        {/* <form onSubmit={handleLogin}>
-          <div>
-            Username <input type='text' value={username} name='Username' onChange={({ target }) => setUsername(target.value)} />
-          </div>
-          <div>
-            Password <input type='password' value={password} name='Password' onChange={({ target }) => setPassword(target.value)} />
-          </div>
-          <button type='submit'>login</button>
-        </form> */}
       </div>
     )
   }
 
   return (
     <div>
-      <h2>Blog list</h2>
+      <h2>Blogs</h2>
+      <p>{user.name} logged in</p>
       {blogs.map((blog) => (
         <Blog key={blog.id} blog={blog} />
       ))}
