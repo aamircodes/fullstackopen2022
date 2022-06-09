64547adab9d9ebcc18d8e647f7ae7c1d9596f474
aamircodes <aamirp1@outlook.com>
2022-06-09 11:42:30 +0100

    5.9

--- part5/src/App.js
+++ part5/src/App.js
@@ -116,9 +116,11 @@ const App = () => {
       {user.name} logged in
       <button onClick={handleLogout}>logout</button>
       {blogForm()}
-      {blogs.map((blog) => (
-        <Blog key={blog.id} blog={blog} addLike={addLike} />
-      ))}
+      {blogs
+        .sort((a, b) => b.likes - a.likes)
+        .map((blog) => (
+          <Blog key={blog.id} blog={blog} addLike={addLike} />
+        ))}
     </div>
   )
 }
