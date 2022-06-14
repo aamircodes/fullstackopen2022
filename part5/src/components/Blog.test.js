883b4c0606755e6fd85ae0f08d5781e35641d837
aamircodes <aamirp1@outlook.com>
2022-06-14 16:46:29 +0100

    5.16

--- part5/src/components/Blog.test.js
+++ part5/src/components/Blog.test.js
@@ -6,6 +6,7 @@ import Blog from './Blog'
 
 describe('Blog renders', () => {
   let container
+  const addLike = jest.fn()
 
   beforeEach(() => {
     const blog = {
@@ -15,7 +16,7 @@ describe('Blog renders', () => {
       likes: 1,
     }
 
-    container = render(<Blog blog={blog} />).container
+    container = render(<Blog blog={blog} addLike={addLike} />).container
   })
 
   test('render title and author', () => {
@@ -26,5 +27,27 @@ describe('Blog renders', () => {
   test('does not render url or likes by default', () => {
     const div = container.querySelector('.blog-details')
     expect(div).toHaveStyle('display: none')
+  })
+
+  test('blog url and lies shown when button has been clicked', async () => {
+    const user = userEvent.setup()
+    const button = screen.getByText('view')
+    await user.click(button)
+
+    const div = container.querySelector('.blog-details')
+    expect(div).not.toHaveStyle('display: none')
+  })
+
+  test('blog event handler called twice after clicking like twice', async () => {
+    const user = userEvent.setup()
+
+    const viewButton = screen.getByText('view')
+    await user.click(viewButton)
+
+    const likeButton = screen.getByText('like')
+    await user.click(likeButton)
+    await user.click(likeButton)
+
+    expect(addLike.mock.calls).toHaveLength(2)
   })
 })
