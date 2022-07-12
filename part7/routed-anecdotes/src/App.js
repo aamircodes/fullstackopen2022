c1cb7d02089a61747d0191a0b278651eb99d61e6
aamircodes <aamirp1@outlook.com>
2022-07-12 12:24:04 +0100

    7.1

--- part7/routed-anecdotes/src/App.js
+++ part7/routed-anecdotes/src/App.js
@@ -1,7 +1,5 @@
-import ReactDOM from 'react-dom/client'
 import { useState } from 'react'
-
-import { BrowserRouter as Router, Routes, Route, Link, Navigate, useParams, useNavigate, useMatch } from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'
 
 const Menu = () => {
   const padding = {
@@ -27,29 +25,11 @@ const AnecdoteList = ({ anecdotes }) => (
     <h2>Anecdotes</h2>
     <ul>
       {anecdotes.map((anecdote) => (
-        <li key={anecdote.id}>
-          <Link to={`/anecdotes/${anecdote.id}`}>{anecdote.content}</Link>
-        </li>
+        <li key={anecdote.id}>{anecdote.content}</li>
       ))}
     </ul>
   </div>
 )
-
-const Anecdote = ({ anecdotes }) => {
-  const id = useParams().id
-  const anecdote = anecdotes.find((a) => a.id === Number(id))
-  return (
-    <div>
-      <h2>
-        {anecdote.content} by {anecdote.author}
-      </h2>
-      <p>has {anecdote.votes} votes </p>
-      <p>
-        for more info see <a href={anecdote.info}>{anecdote.info}</a>
-      </p>
-    </div>
-  )
-}
 
 const About = () => (
   <div>
@@ -154,14 +134,13 @@ const App = () => {
 
   return (
     <div>
+      <h1>Software anecdotes</h1>
       <Router>
-        <h1>Software anecdotes</h1>
         <Menu />
         <Routes>
           <Route path='/' element={<AnecdoteList anecdotes={anecdotes} />} />
           <Route path='/create' element={<CreateNew addNew={addNew} />} />
           <Route path='/about' element={<About />} />
-          <Route path='/anecdotes/:id' element={<Anecdote anecdotes={anecdotes} />} />
         </Routes>
       </Router>
       <Footer />
