4998c07a7034c295f58887d07797799aa724ae5d
aamircodes <aamirp1@outlook.com>
2022-07-14 15:26:31 +0100

    9.7

--- part9/index.ts
+++ part9/index.ts
@@ -1,7 +1,9 @@
 import express from 'express';
 import { calculateBmi } from './bmiCalculator';
+import { calculateExercises } from './exerciseCalculator';
 
 const app = express();
+app.use(express.json());
 
 app.get('/hello', (_req, res) => {
     res.send('Hello Full Stack!');
@@ -21,6 +23,25 @@ app.get('/bmi/:height?:weight?', (req, res) => {
         weight,
         bmi: calculateBmi(weight, height),
     });
+});
+
+app.post('/calculate', (req, res) => {
+    const daily_exercises: Array<number> = req.body.daily_exercises;
+    const target: number = req.body.target;
+
+    if (!daily_exercises || !target) {
+        return res.status(400).send({
+            error: 'parameters missing',
+        });
+    }
+
+    if ([target, ...daily_exercises].some((hours) => isNaN(Number(hours)))) {
+        return res.status(400).send({
+            error: 'malformatted parameters',
+        });
+    }
+
+    return res.send(calculateExercises([target, ...daily_exercises]));
 });
 
 const PORT = 3002;
